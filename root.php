2c1ea8ad29d3087fa510dccdb53972d03b590cc4
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-11-11 13:07:48 +0200

    add postprefix and postinfix function rules

--- root.php
+++ root.php
@@ -1,3 +1,14 @@
 <?php
 
-echo sqrt(a) + sqrt(b);
+{
+    echo sqrt($a) + sqrt($b);
+
+    sqrt(sqrt(sqrt(sqrt($a))));
+    sqrt($a);
+
+    // sqrt();
+    foo();
+    sqrt();
+
+    sqrt($a, $b);
+}
--- square.php
+++ square.php
@@ -4,4 +4,5 @@ function square($n) {
   return $n * $n;
 }
 
+echo "Square of 5 is ";
 echo square(5);
